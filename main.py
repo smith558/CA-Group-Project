e56a332025fba6255b43e0836188db99ae41e4d4
Stanley Modrak <stanislav.modrak@gmail.com>
2020-03-12 19:03:10 +0000

    Add comments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ﻿# coding=utf-8
 import Tkinter as tk
+# AC theory maths calculations module
 import AC_math as ac
 
 
@@ -9,14 +10,16 @@ class Application(tk.Frame):
         self.grid()
         self.create_widgets()
 
+    # main GUI creation method
     def create_widgets(self):
         def create_input_ui(master):
             # initiate text for labels
-            label_text = ['Vs', 'Vr', 'VL', 'Vc', 'XC', 'XL', 'R', 'Z', 'f', 'C', 'L', 'θ', 'Ω', 'Ω', 'Ω', 'Ω', 'Hz',
-                          '°', 'H', 'F', 'V', 'V', 'V', 'V']
-            # dictionary of tk.Label & tk.Entry objects
+            label_texts = ['Vs', 'Vr', 'VL', 'Vc', 'XC', 'XL', 'R', 'Z', 'f', 'C', 'L', 'θ', 'Ω', 'Ω', 'Ω', 'Ω', 'Hz',
+                           '°', 'H', 'F', 'V', 'V', 'V', 'V']
+            # dictionaries of tk.Label & tk.Entry objects
             labels = {}
             entries = {}
+
             # top-window
             top = master.winfo_toplevel()
 
@@ -27,15 +30,16 @@ class Application(tk.Frame):
                 top.columnconfigure(i, weight=1)
                 top.rowconfigure(i, weight=1)
 
-            # set-up labels & entries
-            for i in range(len(label_text)):
+            # set-up labels & Entries
+            for i in range(len(label_texts)):
                 _column = 2 if (i >= 12) else 0
                 _row = i - 12 if (i >= 12) else i
-                print len(label_text)
 
-                label = labels['label_{0}'.format(i)] = tk.Label(master, text=label_text[i])
+                # initiate tk.Label objects in labels[] in format 'label_x' where x is a unique integer from 0 - 23
+                label = labels['label_{0}'.format(i)] = tk.Label(master, text=label_texts[i])
                 label.grid(row=_row, column=_column, ipadx=5, ipady=5, sticky=tk.N + tk.E + tk.S + tk.W)
 
+                # initiate tk.Entry objects in entries[] in format 'entry_x' where x is a unique integer from 0 - 23
                 entry = entries['entry_{0}'.format(i)] = tk.Entry(master)
                 entry.grid(row=_row, column=_column + 1, ipadx=5, ipady=5, sticky=tk.N + tk.E + tk.S + tk.W)
 
@@ -45,6 +49,7 @@ class Application(tk.Frame):
                                   columnspan=width)
 
         create_input_ui(self)
+        # make an empty row
         self.rowconfigure(12, minsize=35, weight=1)
         create_start_btn(self, _row=13, _column=0, width=4)
 

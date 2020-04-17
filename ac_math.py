e3a6651cfa23b7621799fcf7b087c54c511799aa
Stanley Modrak <stanislav.modrak@gmail.com>
2020-04-17 17:46:00 +0200

    Up ac_math

--- ac_math.py
+++ ac_math.py
@@ -1,42 +1,53 @@
 import math
 
 
-def get_resistor_voltage(i, r):
-    vr = i * r
-    return vr
+class RL_CIRCUIT():
+
+    @staticmethod
+    def get_resistor_voltage(i, r):
+        vr = i * r
+        return vr
+
+    @staticmethod
+    def get_supply_voltage(vr, vl):
+        vs = math.sqrt(vr ** 2 + vl ** 2)
+        return vs
+
+    @staticmethod
+    def get_inductor_voltage(i, xl):
+        vl = xl * i
+        return vl
+
+    @staticmethod
+    def get_phase_angle(r, xl, xc):
+        a = ((xl - xc) / r)
+        o = math.atan(a)
+        return o
+
+    @staticmethod
+    def get_inductive_reactance():
+        pass
+
+    @staticmethod
+    def get_circuit_current():
+        pass
+
+    @staticmethod
+    def get_supply_frequency():
+        pass
+
+    @staticmethod
+    def get_circuit_resistance():
+        pass
+
+    @staticmethod
+    def get_circuit_resistance_2():
+        pass
 
 
-def get_supply_voltage(vr, vl):
-    vs = math.sqrt(vr ** 2 + vl ** 2)
-    return vs
-
-
-def get_inductor_voltage(i, xl):
-    vl = xl * i
-    return vl
-
-
-def get_phase_angle(r, xl, xc):
-    a = ((xl - xc) / r)
-    o = math.atan(a)
-    return o
-
-
-def get_inductive_reactance():
+class RC_CIRCUIT():
     pass
 
 
-def get_circuit_current():
-    pass
-
-
-def get_supply_frequency():
-    pass
-
-
-def get_circuit_resistance():
-    pass
-
-
-def get_circuit_resistance_2():
+class RLC_CIRCUIT():
     pass

01f3575457bfead6d979165497924a6f1e2170f0
MGhee <43319500+MGhee@users.noreply.github.com>
2020-04-30 21:08:56 +0300

    Update ac_math.py

--- ac_math.py
+++ ac_math.py
@@ -65,7 +65,64 @@ class RL_CIRCUIT():
 
 # these are TODO
 class RC_CIRCUIT():
-    pass
+    
+    @staticmethod
+    def get_resistor_voltage(i, r):
+        vr = i * r
+        return vr
+
+    @staticmethod
+    def get_supply_voltage(vr, vc):
+        vs = sqrt(vr ** 2 + vc ** 2)
+        return vs
+
+    @staticmethod
+    def get_supply_voltage_2(i, z):
+        return i * z
+
+    @staticmethod
+    def get_capacitor_voltage(i, xc):
+        vc = xc * i
+        return vc
+
+    @staticmethod
+    def get_phase_angle(r, xc):
+        o = atan((1/xc) / r)
+        return o
+
+    @staticmethod
+    def get_phase_angle_2(r, z):
+        o = acos(r / z)
+        return o
+
+    @staticmethod
+    def get_phase_angle_3(xc, z):
+        o = asin((1/xc)/ z)
+        return o
+
+    @staticmethod
+    def get_inductive_reactance(f, c):
+        xc = 1/(2 * pi * f * c)
+        return xc
+
+    @staticmethod
+    def get_circuit_current(z, vs):
+        i = vs / z
+        return i
+
+    @staticmethod
+    def get_supply_frequency(c, xc):
+        f = 1 / (2 * pi * c * xc)
+        return f
+
+    @staticmethod
+    def get_circuit_resistance(z, xc):
+        return abs(sqrt(z ** 2 - (1/xc) ** 2))
+
+    @staticmethod
+    def get_circuit_impedence(xc, r):
+        z = sqrt((1/xc) ** 2 + r ** 2)
+        return z
 
 
 class RLC_CIRCUIT():
